0910b813bb90a258523789015754dc99baeafb4f
csuhan peter <ps.gila456@gmail.com>
2026-01-04 20:19:22 +0000

    Authentikáció

--- Tuzolto-Backend/routes/api.php
+++ Tuzolto-Backend/routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', action: [UserController::class, 'register']);
+
+
+Route::post('/login', [UserController::class, 'login']) ->name('login');
+
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+});
